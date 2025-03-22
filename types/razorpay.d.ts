d28bcc6c60bede8cbbe01b1f9f5f8fd10063c3dd
benak <benakkishanj@gmail.com>
2025-03-22 13:31:11 +0530

    Update razorpay.d.ts

--- types/razorpay.d.ts
+++ types/razorpay.d.ts
@@ -1,28 +1,30 @@
 // types/razorpay.d.ts
-interface RazorpayOptions {
-  key: string;
-  amount: number;
-  currency: string;
-  name: string;
-  description: string;
-  order_id: string;
-  handler: (response: {
-    razorpay_payment_id: string;
-    razorpay_order_id: string;
-    razorpay_signature: string;
-  }) => void;
-  prefill?: { email: string; contact: string };
-  theme?: { color: string };
-}
-
-interface RazorpayInstance {
-  open: () => void;
-  on: (event: string, callback: (response: any) => void) => void;
-}
-
-// Augment the global Window interface
 declare global {
+  interface RazorpayOptions {
+    key: string;
+    amount: number;
+    currency: string;
+    name: string;
+    description: string;
+    order_id: string;
+    handler: (response: {
+      razorpay_payment_id: string;
+      razorpay_order_id: string;
+      razorpay_signature: string;
+    }) => void;
+    prefill?: { email: string; contact: string };
+    theme?: { color: string };
+  }
+
+  interface RazorpayInstance {
+    open: () => void;
+    on: (event: string, callback: (response: any) => void) => void;
+  }
+
   interface Window {
     Razorpay: new (options: RazorpayOptions) => RazorpayInstance;
   }
 }
+
+// This line makes the file a module and merges declarations
+export {};
